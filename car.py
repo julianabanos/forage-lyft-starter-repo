b0f1ddb4db4d69cbb14628e46e42e62a1ed02bfd
Juliana Banos <juliana.banos012@hotmail.com>
2023-02-08 14:51:23 +0800

    Separated battery and engine folders, added Serviceable class and Car Factory class

--- car.py
+++ car.py
@@ -1,12 +1,10 @@
-from abc import ABC, abstractmethod
+from serviceable import Serviceable
 
-
-class Car(ABC):
+class Car(Serviceable):
     def __init__(self, last_service_date, engine, battery):
         self.last_service_date = last_service_date
         self.engine = engine
         self.battery = battery
-
-    @abstractmethod
+        
     def needs_service(self):
         pass
